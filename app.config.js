bdaa98843a617febde91065a884b0d1a3cadf458
yanouyehiel <yanou.yehiel@yahoo.fr>
2025-12-15 09:41:07 +0000

    update

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ module.exports = {
   expo: {
     name: "Sendo",
     slug: "sendo",
-    version: "1.2.5",
+    version: "1.2.6",
     owner: "sfesendo",
     orientation: "portrait",
     icon: "./assets/icon-sendo.png",
